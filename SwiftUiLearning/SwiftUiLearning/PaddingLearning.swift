c1b1ed374c4990469efe2e8eecda34a5ce0713a7
si-jaideep-singh <jaideep.singh@sportzinteractive.net>
2025-04-22 16:43:52 +0530

    Spacer and padding added to stacks

---
+++ SwiftUiLearning/SwiftUiLearning/PaddingLearning.swift
@@ -0,0 +1,18 @@
+//
+//  PaddingLearning.swift
+//  SwiftUiLearning
+//
+//  Created by Jaideep Singh on 22/04/25.
+//
+
+import SwiftUI
+
+struct PaddingLearning: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaddingLearning()
+}
