bc6270dc9283599db418d0b292e4a76d6b7dd147
si-jaideep-singh <jaideep.singh@sportzinteractive.net>
2025-04-22 13:32:17 +0530

    Added custom Images

--- SwiftUiLearning/SwiftUiLearning/Images.swift
+++ SwiftUiLearning/SwiftUiLearning/Images.swift
@@ -9,15 +9,23 @@ import SwiftUI
 
 struct Images: View {
     var body: some View {
-      Image(systemName:
-            "heart.fill")
-      .resizable()
-        
-      .aspectRatio(contentMode: .fit)
-      
-      .frame(width: 200,height: 200)
-      .background(Color.red)
+        //System Icons
+//      Image(systemName:
+//            "heart.fill")
+//      .resizable()
+//        
+//      .aspectRatio(contentMode: .fit)
+//      
+//      .frame(width: 200,height: 200)
+//      .background(Color.red)
        
+        Image("ChennaiSuperKings")
+            .scaledToFit()
+            .clipShape(Rectangle())
+            
+         
+            .frame(width: 300,height: 300,alignment: .center)
+            .background(Color.red)
         
     }
 }
